6b3fa221774b521208d8b313b1d8d59e838a3fc6
Reuben Sammut <reubensammut@gmail.com>
2019-03-01 19:58:34 +0100

    Start implementation of GA functions

--- src/GA.hs
+++ src/GA.hs
@@ -1,9 +1,21 @@
-module GA where
+module GA
+    ( GA(..)
+    , select
+    ) where
 
 import System.Random
 
-class (Random a) => GA a where
-  genRandom :: RandomGen g => g -> a
+class GA a where
+  genRandom :: RandomGen g => g -> (a, g)
   getFitness :: a -> Float
   mate :: a -> a -> a
-  mutate :: RandomGen g => g -> Float -> a -> a
+  mutate :: RandomGen g => g -> Float -> a -> (a, g)
+
+average :: Fractional a => [a] -> a
+average l = sum l / fromIntegral (length l)
+
+select :: GA a => [a] -> [a]
+select pop = map snd . filter (\(a,_) -> a > avg) $ fitnesses
+  where fitnesses = flip zip pop . map getFitness $ pop
+        avg = average . map (fst) $ fitnesses
+
